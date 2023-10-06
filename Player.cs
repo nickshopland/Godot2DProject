4236d3a59a398fd7cc8a11917fb305febdadd4f6
Nick Shopland <nicholas.shopland@ntu.ac.uk>
2023-10-06 09:05:39 +0100

    Initial commit

---
+++ Player.cs
@@ -0,0 +1,80 @@
+using Godot;
+using System;
+
+public partial class Player : Area2D
+{
+	[Signal] public delegate void HitEventHandler();
+	[Export] public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
+	public Vector2 ScreenSize; // Size of the game window.
+	// Called when the node enters the scene tree for the first time.
+
+	public override void _Ready()
+	{
+		ScreenSize = GetViewportRect().Size;
+		Hide();
+	}
+
+	public void Start(Vector2 position)
+	{
+		Position = position;
+		Show();
+		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		// Hide();
+
+		var velocity = Vector2.Zero; // The player's movement vector.
+
+		velocity.X += Input.IsActionPressed("move_right") ? 1 : 0;
+		velocity.X -= Input.IsActionPressed("move_left") ? 1 : 0;
+		velocity.Y += Input.IsActionPressed("move_down") ? 1 : 0;
+		velocity.Y -= Input.IsActionPressed("move_up") ? 1 : 0;
+
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+			animatedSprite2D.Play();
+		}
+		else
+		{
+			animatedSprite2D.Stop();
+		}
+
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+
+		if (velocity.X != 0)
+		{
+			animatedSprite2D.Animation = "walk";
+			animatedSprite2D.FlipV = false;
+			animatedSprite2D.FlipH = velocity.X < 0;
+		}
+		else if (velocity.Y != 0)
+		{
+			animatedSprite2D.Animation = "up";
+			animatedSprite2D.FlipV = velocity.Y > 0;
+		}
+	}
+
+	private void OnBodyEntered(PhysicsBody2D body)
+	{
+		Hide(); // Player disappears after being hit.
+		EmitSignal(SignalName.Hit);
+		// Must be deferred as we can't change physics properties on a physics callback.
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+	}
+
+    private void EmitSignal(object hit)
+    {
+        throw new NotImplementedException();
+    }
+
+}
